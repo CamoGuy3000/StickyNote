3dcbc04fa9d6f5e49578ad6162e9772f70559d03
Devin <dross9431@gmail.com>
2023-10-27 23:52:39 -0400

    Added pinning, made moving not snap mouse, cleaned code

--- extension/content.tsx
+++ extension/content.tsx
@@ -14,9 +14,15 @@ let rect_list = [ ]
 //-look into getting rid of scrolling
 
 
+const MIN_HEIGHT = 50
+const MIN_WIDTH = 50
+const Z_INDEX = '1000'
+const MOVE_Z_INDEX = '1001'
+
 console.log("Content script started")
 
 const rectangle = document.createElement("div");
+
 // rectangle.id = rect_list.length.toString()
 rectangle.id = rect_counter.toString()
 rectangle.setAttribute('stickynote_dragging', 'false')
@@ -39,18 +45,29 @@ rect_counter++
 
 
 rectangle.addEventListener("mousedown", e => {
+  rectangle.style.zIndex = MOVE_Z_INDEX
   let height = parseInt(rectangle.style.height)
   let width = parseInt(rectangle.style.width)
 
-  let clicked_y = e.pageY - parseInt(rectangle.style.top)
-  let clicked_x = e.pageX - parseInt(rectangle.style.left)
+  let top = parseInt(rectangle.style.top)
+  let left = parseInt(rectangle.style.left)
+
+  let clicked_y = e.pageY - top
+  let clicked_x = e.pageX - left
 
   // console.log(clicked_x + ', ' + clicked_y)
   // console.log(height + ', ' + width)
 
   let resize_x = clicked_x > (width - 20)
   let resize_y = clicked_y > (height - 20)
+  let pin = clicked_x < 20 && clicked_y < 20
+  console.log(pin)
+  let style_pos = rectangle.style.position
   switch(true){
+    case pin && style_pos == 'fixed':
+      rectangle.style.position = "absolute"; break
+    case pin && style_pos == 'absolute':
+      rectangle.style.position = "fixed"; break
     case resize_x && !resize_y:
       rectangle.setAttribute('stickynote_resizing', 'horizontal'); break
     case !resize_x && resize_y:
@@ -64,49 +81,47 @@ rectangle.addEventListener("mousedown", e => {
 });
 
 rectangle.addEventListener("mouseup", e => {
-  rectangle.style.zIndex = 'auto'
+  rectangle.style.zIndex = Z_INDEX
   rectangle.setAttribute('stickynote_dragging', 'false')
   rectangle.setAttribute('stickynote_resizing', 'false')
 });
 
 document.addEventListener("mouseup", e => {
   if(rectangle.getAttribute('stickynote_resizing') != 'false'){
-    rectangle.style.zIndex = 'auto'
+    rectangle.style.zIndex = Z_INDEX
     rectangle.setAttribute('stickynote_resizing', 'false')
+  }
+  if(rectangle.getAttribute('stickynote_dragging') != 'false'){
+    rectangle.style.zIndex = Z_INDEX
+    rectangle.setAttribute('stickynote_dragging', 'false')
   }
 });
 
 document.addEventListener("mousemove", e => { 
   // TODO make this not add everytime we create a note, but only the first time on the document
+  window.getSelection()?.removeAllRanges()
   if(rectangle.getAttribute('stickynote_dragging') == 'true'){
-    
-    let x = e.pageX
-    let y = e.pageY
-    
-    let height = parseInt(rectangle.style.height)/2
-    let width = parseInt(rectangle.style.width)/2
-    
-    const top = y - height // should be y - (height of note)
-    const left = x - width // should be x - (width of note)
-    
-    // console.log('Stuff:')
-    // console.log(height + ' ' + width)
-    // console.log(y + ' ' + x)
-    // console.log(top + ' ' + left)
-    
+    let dx = e.movementX
+    let dy = e.movementY
+    let prev_top = parseInt(rectangle.style.top)
+    let prev_left = parseInt(rectangle.style.left)
+    let top = prev_top + dy
+    let left = prev_left + dx
     rectangle.style.top = top + "px"
     rectangle.style.left = left + "px"
   }
   else if(rectangle.getAttribute('stickynote_resizing') != 'false'){
+    window.getSelection()?.removeAllRanges()
+
     let resize_type = rectangle.getAttribute('stickynote_resizing')
 
     let height = e.pageY - parseInt(rectangle.style.top)
     let width = e.pageX - parseInt(rectangle.style.left)
     // console.log(height + ' ' + width)
-    if(height < 10)
-      height = 10
-    if(width < 10)
-      width = 10
+    if(height < MIN_HEIGHT)
+      height = MIN_HEIGHT
+    if(width < MIN_WIDTH)
+      width = MIN_WIDTH
 
     switch(resize_type){
       case 'horizontal':
